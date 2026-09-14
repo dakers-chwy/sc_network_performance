d2e92c08a9cde65870e414eb777261ce02137945
Darnell Akers <dakers@chewy.com>
2026-09-14 08:56:54 -0700

    Delete outbound-bi-repo-template/sql/shared/calendar_example.sql

--- outbound-bi-repo-template/sql/shared/calendar_example.sql
+++
@@ -1,9 +0,0 @@
--- Synthetic Snowflake example: Saturday week ending independent of WEEK_START.
-WITH dates AS (
-    SELECT column1::DATE AS reporting_date
-    FROM VALUES ('2026-09-12'), ('2026-09-13'), ('2026-09-14')
-)
-SELECT reporting_date,
-       DATEADD(DAY, MOD(6 - DAYOFWEEKISO(reporting_date) + 7, 7), reporting_date)::DATE AS weekending_date
-FROM dates
-ORDER BY reporting_date;
